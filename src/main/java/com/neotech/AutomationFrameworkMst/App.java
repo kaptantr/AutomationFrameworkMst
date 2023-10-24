25fc22fbdcf316ee48b7b11ccc4ace2239adf7a9
Mesut EROL <mesut_erol_tr@hotmail.com>
2023-10-24 15:16:26 -0400

    First Start Maven Project

---
+++ src/main/java/com/neotech/AutomationFrameworkMst/App.java
@@ -0,0 +1,13 @@
+package com.neotech.AutomationFrameworkMst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
